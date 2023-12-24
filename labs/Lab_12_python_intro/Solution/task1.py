57463c453678e3b8abea53f85545cdc96b44f303
NikitaGiss <150254069+NikitaGiss@users.noreply.github.com>
2023-12-25 00:26:51 +0300

    Update task1.py

--- labs/Lab_12_python_intro/Solution/task1.py
+++ labs/Lab_12_python_intro/Solution/task1.py
@@ -1,11 +1,17 @@
-def quantity(N, M):
-    board = [[0] * M for _ in range(N)]
-    board[0][0] = 1
+with open('input.txt', 'r') as file:
+    N, M = map(int, file.readline().split())
+    if not (1 <= N <= 50 and 1 <= M <= 50):
+        print("Нужны числа от 1 до 50!! измените числа в входном файле и перезапустите код.")
+
+    dp = [[0] * M for _ in range(N)]
+    dp[0][0] = 1
+
     for i in range(N):
         for j in range(M):
-            board[i][j] += board[i - 2][j - 1] if i - 2 >= 0 and j - 1 >= 0 else 0
-            board[i][j] += board[i - 1][j - 2] if i - 1 >= 0 and j - 2 >= 0 else 0
-    return board[N - 1][M - 1]
-if __name__ == '__main__':
-    N, M = map(int, input().split())
-    print(quantity(N, M))
+            if i + 1 < N and j + 2 < M:
+                dp[i+1][j+2] += dp[i][j]
+            if i + 2 < N and j + 1 < M:
+                dp[i+2][j+1] += dp[i][j]
+
+    result = dp[N-1][M-1]
+    print(result)
