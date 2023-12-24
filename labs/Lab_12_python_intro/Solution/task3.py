b60d38f2764e796a42baa27a5fc5b640a5829de2
NikitaGiss <150254069+NikitaGiss@users.noreply.github.com>
2023-12-25 00:27:56 +0300

    Update task3.py

--- labs/Lab_12_python_intro/Solution/task3.py
+++ labs/Lab_12_python_intro/Solution/task3.py
@@ -1,26 +1,23 @@
+with open('input.txt', 'r') as file:
+    text = file.read()
+    char_count = {}
+    for char in text:
+        if char not in char_count and char != ' ' and char != '\n':
+            char_count[char] = 1
+        elif char != ' ' and char != '\n':
+            char_count[char] += 1
 
-data = input()
-characters_count = {}
-max_count = 1
-for line in data:
-    line = line.strip()
-    for char in line:
-        if char in characters_count:
-            characters_count[char] += 1
-            max_count = max(max_count, characters_count[char])
-        else:
-            characters_count[char] = 1
-unique_chars = sorted(set(characters_count.keys()))
+    sorted_chars = sorted(char_count.keys())
+    max_count = max(char_count.values())
 
-for i in range(max_count, 0, -1):
-    line = ''
-    for char in unique_chars:
-        if characters_count.get(char, 0) >= i:
-            line += '#'
-        else:
-            line += ' '
-    print(line)
+    for i in range(max_count, 0, -1):
+        line = ''
+        for char in sorted_chars:
+            if char_count.get(char, 0) >= i:
+                line += '#'
+            else:
+                line += ' '
+        print(line)
 
-for char in unique_chars:
-    print(char, end=' ')
-
+    symbols_line = ''.join(sorted_chars)
+    print(symbols_line)
